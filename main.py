32a2c84f343fa44a6c461dd3c829754c2320630c
pirxcy's random work <738777@newcollege.ac.uk>
2025-10-14 14:13:22 +0100

    Update main.py to work with logger.py
    Logging basic events

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import sanic
 from sanic.exceptions import NotFound
 from sanic import response
 from users import Users as users
+from logger import logs
 from utils import render_template
 
 app = sanic.Sanic("test")
+log = logs(app_name="Coffee Shop", file_name="app.log")
 
 
-
-@app.exception(NotFound)
-async def catch404(request:sanic.Request,exception:sanic.SanicException):
+@app.exception(NotFound)  # type:ignore
+async def catch404(request: sanic.Request, exception: sanic.SanicException):
+    log.add(f"User requested {request.path}, redirecting to main page.")
     return response.redirect("/")
-    
-    
+
 
 @app.route("/", methods=["GET", "POST"])
 async def index(request: sanic.Request) -> sanic.HTTPResponse:
     if request.method == "GET":
+        log.add("Index page viewed.")
         return render_template("index.html")
     elif request.method == "POST":
         if request.form:
@@ -41,17 +43,20 @@ async def index(request: sanic.Request) -> sanic.HTTPResponse:
                 email=email,  # type:ignore (sanic's typing is shamoblic so i have to spam this)
                 message=message,  # type:ignore (sanic's typing is shamoblic so i have to spam this)
             )
+            log.add(f"{email} stored")
 
         return response.redirect("/")
     else:
         return response.empty(status=404)
 
 
-@app.route("/users",methods=["GET","POST"])
+@app.route("/users", methods=["GET", "POST"])
 async def users_page(request: sanic.Request) -> sanic.HTTPResponse:
     if request.method == "GET":
+        log.add("Users Viewed")
         return render_template("users.html")
     elif request.method == "POST":
+        log.add("Users Requested")
         return response.json(users.view())
     else:
         return response.empty(status=404)
